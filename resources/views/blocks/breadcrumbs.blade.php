6584937adb79f01e867d327c37dc5e39035daa3d
AlphaTrauma <deloriet@gmail.com>
2022-04-15 16:32:36 +0500

    f

--- resources/views/blocks/breadcrumbs.blade.php
+++ resources/views/blocks/breadcrumbs.blade.php
@@ -1,6 +1,6 @@
 @if(Route::currentRouteName() !== 'main')
 <ul class="uk-breadcrumb">
-        <li><a href="{{ route('main') }}">Главная</a></li>
+        <li><a href="{{ route('main') }}">Тюменский учебный центр</a></li>
     @isset($item->alias)
         <li><a href="{{ $item->alias['path'] }}">{{ $item->alias['title'] }}</a></li>
     @endisset
