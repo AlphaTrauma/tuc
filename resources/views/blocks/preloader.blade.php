955f5cb62ce3b4dcf6f11ce80488c834bffc1efb
AlphaTrauma <deloriet@gmail.com>
2022-05-03 16:54:34 +0500

    f

--- resources/views/blocks/preloader.blade.php
+++ resources/views/blocks/preloader.blade.php
@@ -1,4 +1,4 @@
-<div id="preloader" class="uk-background-default uk-position-z-index uk-position-cover">
+<div id="preloader" class="uk-background-default uk-position-z-index uk-position-cover uk-overflow-hidden">
     <div class="uk-position-center">
         <span uk-spinner="ratio: 3"></span>
         <p class="uk-text-center">Загрузка</p>
