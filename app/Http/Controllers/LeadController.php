75131ae13440019fd4359326b55960f6933f186f
AlphaTrauma <deloriet@gmail.com>
2022-05-18 16:57:29 +0500

    leads to tbot

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -24,6 +24,19 @@ class LeadController extends Controller
     {
         $data = $request->all();
         Lead::create($data);
+        $lead = 'Новая заявка с сайта: '.
+                '*Телефон:* '.$data['phone'].PHP_EOL.
+                '*E-mail:* '.$data['email'].PHP_EOL.
+                '*Имя:* '.$data['name'].PHP_EOL.
+                '*Страница:* '.$data['page'].PHP_EOL.
+                '*Курс:* '.$data['course'].PHP_EOL.
+                '*Сообщение:* '.$data['comment'];
+        $data = [
+            'chat_id' => '-1001708032534',
+            'text' => $lead
+        ];
+        $response = file_get_contents("https://api.telegram.org/bot5344836009:AAGH0z3JJdlfN10sNjK_457a_2C_mFrNc1k/sendMessage?".
+            http_build_query($data) );
 
         return redirect()->route('lead.create')->with('message', 'Ваша заявка успешно отправлена');
 
