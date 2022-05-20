b317250cfcfad18f7feddf816d5cf7176ea64f05
AlphaTrauma <deloriet@gmail.com>
2022-05-20 15:45:29 +0500

    Update preloader.blade.php

--- resources/views/blocks/preloader.blade.php
+++ resources/views/blocks/preloader.blade.php
@@ -1,4 +1,4 @@
-<div id="preloader" class="uk-background-default uk-position-z-index uk-position-cover uk-overflow-hidden">
+<div id="preloader" class="uk-background-default uk-position-fixed uk-position-cover uk-overflow-hidden" style="z-index: 10000">
     <div class="uk-position-center">
         <span uk-spinner="ratio: 3"></span>
         <p class="uk-text-center">Загрузка</p>
