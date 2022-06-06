17988d899fd616fab4df2ff98eb9684a38fe66a9
AlphaTrauma <deloriet@gmail.com>
2022-06-06 08:50:48 +0500

    f

--- database/migrations/new/2022_06_03_104320_create_user_materials_table.php
+++ database/migrations/new/2022_06_03_104320_create_user_materials_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMaterialLinksTable extends Migration
+class CreateUserMaterialsTable extends Migration
 {
     /**
      * Run the migrations.
