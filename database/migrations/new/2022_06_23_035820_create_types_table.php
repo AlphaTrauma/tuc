70c35de0a5eca5d77dc55940692e01950a85ff16
AlphaTrauma <deloriet@gmail.com>
2022-06-24 09:32:35 +0500

    db

---
+++ database/migrations/new/2022_06_23_035820_create_types_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('slug')->nullable();
+            $table->integer('ordering')->default(0);
+            $table->text('description')->nullable();
+            $table->boolean('status')->default(true);
+            $table->text('html')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+}
