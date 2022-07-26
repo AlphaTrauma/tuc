8480618565b4cb286c1a3f556e534dd6891fafe2
AlphaTrauma <deloriet@gmail.com>
2022-07-26 19:32:23 +0500

    fix

--- resources/views/dashboard/materials/test.blade.php
+++ resources/views/dashboard/materials/test.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('title')
-    Тестовая
+    Конструктор теста
 @endsection
 
 @section('content')
--- resources/views/dashboard/test.blade.php
+++ resources/views/dashboard/test.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.dashboard')
 
 @section('title')
-    Тестовая
+    Конструктор теста
 @endsection
 
 @section('content')
-    <h1>Тестовая страница</h1>
+    <h1>Конструктор теста</h1>
     <div class="uk-padding uk-padding-remove-vertical">
         <test-container></test-container>
     </div>
