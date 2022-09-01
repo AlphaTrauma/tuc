bf84c8614e283bccc2475c14de0f9e1a9195293f
AlphaTrauma <deloriet@gmail.com>
2022-09-01 09:28:15 +0500

    Update pdf.blade.php

--- resources/views/personal/courses/materials/pdf.blade.php
+++ resources/views/personal/courses/materials/pdf.blade.php
@@ -9,6 +9,10 @@
     </h2>
     <p>{{ $material->description }}</p>
 
+    <div class="uk-padding uk-padding-remove-horizontal uk-hidden@m">
+        <a href="{{ asset($material->document->filepath) }}" class="uk-button uk-button-primary">Открыть pdf</a>
+    </div>
+
     <div class="uk-padding uk-padding-remove-horizontal">
         <embed class="uk-width-1-1 uk-height-viewport" src="{{ asset($material->document->filepath) }}" alt="pdf" pluginspage="http://www.adobe.com/products/acrobat/readstep2.html">
     </div>
